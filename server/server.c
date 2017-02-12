95ab6cd5f56f61ba7ecbbca74e5366bb33bf120a
sammanthp007 <samman.thapa@bison.howard.edu>
2017-02-12 16:34:52 -0500

    server.c:message on empty file

--- server/server.c
+++ server/server.c
@@ -131,6 +131,7 @@ int main(int argc, char *argv[])
                     strcpy(msg, filecontent);
                 }
                 else {
+                    printf("empty file here");
                     strcpy(msg, "");
                 }
 
