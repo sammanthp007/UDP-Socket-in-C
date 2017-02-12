48af000d96d43c4751de466a7eceb9a80b42facc
sammanthp007 <samman.thapa@bison.howard.edu>
2017-02-12 05:03:36 -0500

    client.c: added the code for handling files from TCP assignment

--- client.c
+++ client.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
         if (*endptr) {
             print_error_and_exit("Invalid client TCPport supplied");
         }
-        
+
         // get server UDP port
         server_UDP_port = strtol(argv[3], &endptr, 0);
         if (*endptr) {
@@ -41,10 +41,10 @@ int main(int argc, char *argv[])
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(server_UDP_port);
-    
+
     /* Set server ip */
 
-     if (inet_aton(argv[2], &server_addr.sin_addr) <= 0) {
+    if (inet_aton(argv[2], &server_addr.sin_addr) <= 0) {
         print_error_and_exit("Setting UDP IP server");
     }
 
@@ -70,10 +70,10 @@ int main(int argc, char *argv[])
             // Send message
             // Create a new sending buffer whose size can take in all msg
             char sending_msg[buffer_len + 5];
-          
+
             // add CAP\n
             strcpy(sending_msg, "CAP\n");
-            
+
             // buffer already has the trailing \n from fgets
             strcat(sending_msg, input_buffer);
 
@@ -93,6 +93,53 @@ int main(int argc, char *argv[])
             {
                 sending_msg[n] = '\0';
                 write(1, sending_msg, n);
+            }
+        }
+        else if (strlen(input_buffer) == 2 && strncmp(input_buffer, "t", 1) == 0){
+            printf("Enter file name: \n");
+            // fgets also takes in the last \n
+            fgets(input_buffer, MAX_LINE, stdin);
+
+            // Save the name of the file for future use
+            char file_name[MAX_LINE];
+            strcpy(file_name, input_buffer);
+            int buffer_len = strlen(input_buffer);
+            file_name[buffer_len - 1] = '\0';
+
+            /* Send message to server in the form "FILE\nxxx\n" */
+            char message[MAX_LINE + 10];
+
+            strcpy(message, "FILE\n");
+            strcat(message, input_buffer);
+
+            message[buffer_len + 5] = '\0';
+
+            if (n = sendto(c_udp_soc, message, strlen(message), 0, (struct sockaddr *)&server_addr, len_serv_addr) < 0)
+            {
+                print_error_and_exit("sending");
+            }
+
+            /* receive message from server */
+            if ((n = recvfrom(c_udp_soc, message, MAX_LINE, 0, (struct sockaddr *)&server_addr, &len_serv_addr)) > 0)
+            {
+                // because noise gets added during transmission
+                message[n] = '\0';
+                write(1, message, n);
+
+
+                // Find the just data section of the message
+                int ii = strcspn(message, "\n");
+                char just_data[n];
+                memcpy(just_data, &message[ii + 1], n - ii);
+
+                //save the just the data to a file
+                FILE* dat = fopen(file_name, "wb");
+
+                fprintf(dat, just_data);
+                fclose(dat);
+
+                printf("Server has responded. The content of server response");
+                printf(" has been saved in %s successfully.\n", file_name);
             }
         }
         else if (strlen(input_buffer) == 2 && strncmp(input_buffer, "q", 1) == 0)
